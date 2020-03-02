9e10bcc4721995c1f78634a50b5a220e61e09b81
Tommy W <woot@threescreenstudios.com>
2020-03-02 11:16:35 -0500

    Add translations, backing vars for first two pages of i589

--- vue-frontend/src/views/i589/subforms/page1.vue
+++ vue-frontend/src/views/i589/subforms/page1.vue
@@ -1,13 +1,29 @@
+<i18n>
+	{
+	"en": {
+	"form-name": "Registration Numbers",
+	"alien-reg-num": "Alien Registration Number(s)",
+	"us-ssn": "U.S. Social Security Number",
+	"uscis-acctnum" : "USCIS Online Account Number"
+	},
+	"es": {
+	"form-name": "Números de registro",
+	"alien-reg-num": "Número (s) de registro de extranjero",
+	"us-ssn": "Número de Seguro Social EE.UU",
+	"uscis-acctnum": "Número de cuenta en línea de USCIS"
+	}
+	}
+</i18n>
 <template>
 	<div>
-		<h4>Registration Numbers</h4>
-		<div class="form-group"><label>Alien Registration Number(s)</label><input name="alien-registration-number"
-				type="text" class="form-control undefined" value="">
+		<h4>{{$t("form-name")}}</h4>
+		<div class="form-group"><label>{{$t("alien-reg-num")}}</label><input v-model="alienRegNum" type="text"
+				class="form-control" value="">
 		</div>
-		<div class="form-group"><label>U.S. Social Security Number</label><input name="us-ssn" type="text"
-				class="form-control undefined" value=""></div>
-		<div class="form-group"><label>USCIS Online Account Number</label><input name="uscis-acct-number" type="text"
-				class="form-control undefined" value=""></div>
+		<div class="form-group"><label>{{$t("us-ssn")}}</label><input v-model="usSsn" type="text" class="form-control"
+				value=""></div>
+		<div class="form-group"><label>{{$t("uscis-acctnum")}}</label><input v-model="uscisAcctNum" type="text"
+				class="form-control" value=""></div>
 	</div>
 </template>
 
@@ -20,7 +36,9 @@
 	// Define the component in class-style
 	@Component
 	export default class page1 extends Vue {
-
+		alienRegNum = "";
+		usSsn = "";
+		uscisAcctNum = "";
 	}
 </script>
 
--- vue-frontend/src/views/i589/subforms/page2.vue
+++ vue-frontend/src/views/i589/subforms/page2.vue
@@ -1,15 +1,33 @@
+<i18n>
+	{
+	"en": {
+	"form-name": "Your Name",
+	"last-name": "Last Name",
+	"first-name": "First Name",
+	"middle-name" : "Middle Name",
+	"other-names" : "Other Names"
+	},
+	"es": {
+	"form-name": "Tu Nombre",
+	"last-name": "Apellido",
+	"first-name": "Nombre de pila",
+	"middle-name": "Segundo nombre",
+	"other-names": "Otros nombres"
+	}
+	}
+</i18n>
 <template>
 	<div>
-		<h4>Your Name</h4>
-		<div class="form-group"><label>Last Name</label><input name="last-name" type="text"
-				class="form-control undefined" value="">
+		<h4>{{$t("form-name")}}</h4>
+		<div class="form-group"><label>{{$t("last-name")}}</label><input v-model="lastName" type="text"
+				class="form-control" value="">
 		</div>
-		<div class="form-group"><label>First Name</label><input name="first-name" type="text"
-				class="form-control undefined" value=""></div>
-		<div class="form-group"><label>Middle Name</label><input name="middle-name" type="text"
-				class="form-control undefined" value=""></div>
-		<div class="form-group"><label>Other names</label><input name="other-names" type="text"
-				class="form-control undefined" value=""></div>
+		<div class="form-group"><label>{{$t("first-name")}}</label><input v-model="firstName" type="text"
+				class="form-control" value=""></div>
+		<div class="form-group"><label>{{$t("middle-name")}}</label><input v-model="middleName" type="text"
+				class="form-control" value=""></div>
+		<div class="form-group"><label>{{$t("other-names")}}</label><input v-model="otherNames" type="text"
+				class="form-control" value=""></div>
 	</div>
 </template>
 
@@ -22,7 +40,10 @@
 	// Define the component in class-style
 	@Component
 	export default class page2 extends Vue {
-
+		lastName = "";
+		firstName = "";
+		middleName = "";
+		otherNames = "";
 	}
 </script>
 
