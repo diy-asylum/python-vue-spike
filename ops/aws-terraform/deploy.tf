0fa8007027533698c2a0d91bd942e11a2ea11726
mkreisel <michael.c.kreisel@gmail.com>
2020-03-15 01:05:01 -0400

    reduce costs by removing nat gateway

--- ops/aws-terraform/deploy.tf
+++ ops/aws-terraform/deploy.tf
@@ -33,7 +33,7 @@ EOF
 
 resource "aws_iam_role_policy" "ecs_execution_policy" {
   name = "ecs_execution_policy"
-  role = "${aws_iam_role.ecs_execution_role.id}"
+  role = aws_iam_role.ecs_execution_role.id
 
   policy = <<EOF
 {
@@ -60,7 +60,7 @@ EOF
 resource "aws_security_group" "egress_all" {
   name        = "egress_all"
   description = "Allow all outbound traffic"
-  vpc_id      = "${aws_vpc.main.id}"
+  vpc_id      = aws_vpc.main.id
 
   egress {
     from_port   = 0
@@ -73,7 +73,7 @@ resource "aws_security_group" "egress_all" {
 resource "aws_security_group" "be_lb_security" {
   name        = "be_lb_security"
   description = "Allow all inbound HTTPS traffic"
-  vpc_id      = "${aws_vpc.main.id}"
+  vpc_id      = aws_vpc.main.id
 
   ingress {
     from_port   = 443
@@ -95,23 +95,23 @@ variable "backend_port" {}
 resource "aws_security_group" "be_ecs_security" {
   name        = "be_ecs_security"
   description = "Allow inbound traffic from BE load balancer"
-  vpc_id      = "${aws_vpc.main.id}"
+  vpc_id      = aws_vpc.main.id
 
   ingress {
-    from_port       = "${var.backend_port}"
-    to_port         = "${var.backend_port}"
+    from_port       = var.backend_port
+    to_port         = var.backend_port
     protocol        = "tcp"
-    security_groups = ["${aws_security_group.be_lb_security.id}"]
+    security_groups = [aws_security_group.be_lb_security.id]
   }
 }
 
 resource "aws_security_group_rule" "traffic_from_be_lb_to_ecs" {
   type                     = "egress"
-  from_port                = "${var.backend_port}"
-  to_port                  = "${var.backend_port}"
+  from_port                = var.backend_port
+  to_port                  = var.backend_port
   protocol                 = "tcp"
-  source_security_group_id = "${aws_security_group.be_ecs_security.id}"
-  security_group_id        = "${aws_security_group.be_lb_security.id}"
+  source_security_group_id = aws_security_group.be_ecs_security.id
+  security_group_id        = aws_security_group.be_lb_security.id
 }
 
 # Create private VPC to host our service.
@@ -126,85 +126,41 @@ resource "aws_vpc" "main" {
 # Corresponding public subnets will be used for public facing load balancers
 # and for NAT gateways which give our servers the ability to access the public internet.
 resource "aws_subnet" "public_subnet_1" {
-  vpc_id                  = "${aws_vpc.main.id}"
+  vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.0.0/26"
   map_public_ip_on_launch = "true"
   availability_zone       = "us-east-1a"
 }
 
 resource "aws_subnet" "public_subnet_2" {
-  vpc_id                  = "${aws_vpc.main.id}"
+  vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.0.64/26"
   map_public_ip_on_launch = "true"
   availability_zone       = "us-east-1b"
 }
 
-resource "aws_subnet" "private_subnet_1" {
-  vpc_id                  = "${aws_vpc.main.id}"
-  cidr_block              = "10.0.0.128/26"
-  map_public_ip_on_launch = "false"
-  availability_zone       = "us-east-1a"
-}
-
-resource "aws_subnet" "private_subnet_2" {
-  vpc_id                  = "${aws_vpc.main.id}"
-  cidr_block              = "10.0.0.192/26"
-  map_public_ip_on_launch = "false"
-  availability_zone       = "us-east-1b"
-}
-
 # Internet gateway to give our public subnets access to the internet
 resource "aws_internet_gateway" "gw" {
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id = aws_vpc.main.id
 }
 
 resource "aws_route_table" "public_egress" {
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.gw.id}"
+    gateway_id = aws_internet_gateway.gw.id
   }
 }
 
 resource "aws_route_table_association" "public_egress_route_1" {
-  subnet_id      = "${aws_subnet.public_subnet_1.id}"
-  route_table_id = "${aws_route_table.public_egress.id}"
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_egress.id
 }
 
 resource "aws_route_table_association" "public_egress_route_2" {
-  subnet_id      = "${aws_subnet.public_subnet_2.id}"
-  route_table_id = "${aws_route_table.public_egress.id}"
-}
-
-# NAT gateway to give our private subnets access to the public internet
-resource "aws_eip" "public_eip_1" {
-  vpc = true
-}
-
-resource "aws_nat_gateway" "nat" {
-  subnet_id     = "${aws_subnet.public_subnet_1.id}"
-  allocation_id = "${aws_eip.public_eip_1.id}"
-  depends_on    = ["aws_internet_gateway.gw"]
-}
-
-resource "aws_route_table" "nat_route" {
-  vpc_id = "${aws_vpc.main.id}"
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_nat_gateway.nat.id}"
-  }
-}
-
-resource "aws_route_table_association" "subnet_1_association" {
-  subnet_id      = "${aws_subnet.private_subnet_1.id}"
-  route_table_id = "${aws_route_table.nat_route.id}"
-}
-
-resource "aws_route_table_association" "subnet_2_association" {
-  subnet_id      = "${aws_subnet.private_subnet_2.id}"
-  route_table_id = "${aws_route_table.nat_route.id}"
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_egress.id
 }
 
 # Create target groups for load balancing.
@@ -212,18 +168,18 @@ resource "aws_route_table_association" "subnet_2_association" {
 
 resource "aws_lb_target_group" "be-target-group" {
   name        = "diyasylum-be"
-  port        = "${var.backend_port}"
+  port        = var.backend_port
   protocol    = "HTTP"
   target_type = "ip"
-  vpc_id      = "${aws_vpc.main.id}"
+  vpc_id      = aws_vpc.main.id
 }
 
 # Create load balancers.
 # Note that the BE load balancer is in the public subnets
 resource "aws_lb" "be-load-balancer" {
   name            = "be-load-balancer"
-  security_groups = ["${aws_security_group.be_lb_security.id}"]
-  subnets         = ["${aws_subnet.public_subnet_1.id}", "${aws_subnet.public_subnet_2.id}"]
+  security_groups = [aws_security_group.be_lb_security.id]
+  subnets         = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
   internal        = false
 
   tags = {
@@ -241,20 +197,20 @@ resource "aws_route53_zone" "diyasylum_private" {
   name = "diyasylum.com"
 
   vpc {
-    vpc_id = "${aws_vpc.main.id}"
+    vpc_id = aws_vpc.main.id
   }
 }
 
 variable "be_dns" {}
 
 resource "aws_route53_record" "public_be_dev_dns" {
-  zone_id = "${data.aws_route53_zone.diyasylum_public.zone_id}"
-  name    = "${var.be_dns}"
+  zone_id = data.aws_route53_zone.diyasylum_public.zone_id
+  name    = var.be_dns
   type    = "A"
 
   alias {
-    name                   = "${aws_lb.be-load-balancer.dns_name}"
-    zone_id                = "${aws_lb.be-load-balancer.zone_id}"
+    name                   = aws_lb.be-load-balancer.dns_name
+    zone_id                = aws_lb.be-load-balancer.zone_id
     evaluate_target_health = true
   }
 }
@@ -267,42 +223,42 @@ resource "aws_acm_certificate" "diyasylum_cert" {
 }
 
 resource "aws_route53_record" "diyasylum_cert_validation" {
-  name    = "${aws_acm_certificate.diyasylum_cert.domain_validation_options.0.resource_record_name}"
-  type    = "${aws_acm_certificate.diyasylum_cert.domain_validation_options.0.resource_record_type}"
-  zone_id = "${data.aws_route53_zone.diyasylum_public.zone_id}"
-  records = ["${aws_acm_certificate.diyasylum_cert.domain_validation_options.0.resource_record_value}"]
+  name    = aws_acm_certificate.diyasylum_cert.domain_validation_options.0.resource_record_name
+  type    = aws_acm_certificate.diyasylum_cert.domain_validation_options.0.resource_record_type
+  zone_id = data.aws_route53_zone.diyasylum_public.zone_id
+  records = [aws_acm_certificate.diyasylum_cert.domain_validation_options.0.resource_record_value]
   ttl     = 60
 }
 
 resource "aws_acm_certificate_validation" "diyasylum_cert_validation" {
-  certificate_arn         = "${aws_acm_certificate.diyasylum_cert.arn}"
-  validation_record_fqdns = ["${aws_route53_record.diyasylum_cert_validation.fqdn}"]
+  certificate_arn         = aws_acm_certificate.diyasylum_cert.arn
+  validation_record_fqdns = [aws_route53_record.diyasylum_cert_validation.fqdn]
 }
 
 # Direct load balancers to forward to target groups.
 # BE redirects HTTP requests to HTTPS
 resource "aws_lb_listener" "be-listener" {
-  load_balancer_arn = "${aws_lb.be-load-balancer.arn}"
+  load_balancer_arn = aws_lb.be-load-balancer.arn
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2015-05"
-  certificate_arn   = "${aws_acm_certificate.diyasylum_cert.arn}"
+  certificate_arn   = aws_acm_certificate.diyasylum_cert.arn
 
   default_action {
-    target_group_arn = "${aws_lb_target_group.be-target-group.arn}"
+    target_group_arn = aws_lb_target_group.be-target-group.arn
     type             = "forward"
   }
 
-  depends_on = ["aws_acm_certificate_validation.diyasylum_cert_validation"]
+  depends_on = [aws_acm_certificate_validation.diyasylum_cert_validation]
 }
 
 resource "aws_lb_listener" "be-redirect-http-to-https" {
-  load_balancer_arn = "${aws_lb.be-load-balancer.arn}"
+  load_balancer_arn = aws_lb.be-load-balancer.arn
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
-    target_group_arn = "${aws_lb_target_group.be-target-group.arn}"
+    target_group_arn = aws_lb_target_group.be-target-group.arn
     type             = "redirect"
 
     redirect {
@@ -315,7 +271,7 @@ resource "aws_lb_listener" "be-redirect-http-to-https" {
 
 # Print load balancer DNS when deployment is complete.
 output "be_dns" {
-  value = "${aws_route53_record.public_be_dev_dns.name}"
+  value = aws_route53_record.public_be_dev_dns.name
 }
 
 # Define parameters of ECS task. Indicates the Docker container which will be run,
@@ -326,7 +282,7 @@ resource "aws_ecs_task_definition" "diyasylum-be" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = 256
   memory                   = 512
-  execution_role_arn       = "${aws_iam_role.ecs_execution_role.arn}"
+  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
 
   container_definitions = <<DEFINITION
 [
@@ -344,7 +300,7 @@ resource "aws_ecs_task_definition" "diyasylum-be" {
 ]
 DEFINITION
 
-  depends_on = ["aws_iam_role_policy.ecs_execution_policy"]
+  depends_on = [aws_iam_role_policy.ecs_execution_policy]
 }
 
 # Create ECS cluster.
@@ -357,25 +313,25 @@ resource "aws_ecs_cluster" "diyasylum" {
 # The BE service is publicly accesible via the load balancer defined above.
 resource "aws_ecs_service" "diyasylum-be" {
   name          = "diyasylum-be"
-  cluster       = "${aws_ecs_cluster.diyasylum.id}"
+  cluster       = aws_ecs_cluster.diyasylum.id
   desired_count = 2
   launch_type   = "FARGATE"
 
   network_configuration {
-    assign_public_ip = false
-    security_groups  = ["${aws_security_group.egress_all.id}", "${aws_security_group.be_ecs_security.id}"]
-    subnets          = ["${aws_subnet.private_subnet_1.id}", "${aws_subnet.private_subnet_2.id}"]
+    assign_public_ip = true
+    security_groups  = [aws_security_group.egress_all.id, aws_security_group.be_ecs_security.id]
+    subnets          = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
   }
 
-  task_definition = "${aws_ecs_task_definition.diyasylum-be.family}"
+  task_definition = aws_ecs_task_definition.diyasylum-be.family
 
   load_balancer {
-    target_group_arn = "${aws_lb_target_group.be-target-group.arn}"
+    target_group_arn = aws_lb_target_group.be-target-group.arn
     container_name   = "diyasylum-be"
-    container_port   = "${var.backend_port}"
+    container_port   = var.backend_port
   }
 
-  depends_on = ["aws_lb_listener.be-listener"]
+  depends_on = [aws_lb_listener.be-listener]
 }
 
 # FE deployment
@@ -407,7 +363,7 @@ EOF
 }
 
 output "fe_s3_dns" {
-  value = "${aws_s3_bucket.fe_bucket.website_endpoint}"
+  value = aws_s3_bucket.fe_bucket.website_endpoint
 }
 
 locals {
@@ -417,8 +373,8 @@ locals {
 # Cloudfront distribution
 resource "aws_cloudfront_distribution" "fe_cloudfront" {
   origin {
-    domain_name = "${aws_s3_bucket.fe_bucket.website_endpoint}"
-    origin_id   = "${local.s3_origin_id}"
+    domain_name = aws_s3_bucket.fe_bucket.website_endpoint
+    origin_id   = local.s3_origin_id
 
     custom_origin_config {
       http_port = "80"
@@ -435,7 +391,7 @@ resource "aws_cloudfront_distribution" "fe_cloudfront" {
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "${local.s3_origin_id}"
+    target_origin_id = local.s3_origin_id
 
     forwarded_values {
       query_string = false
@@ -458,15 +414,17 @@ resource "aws_cloudfront_distribution" "fe_cloudfront" {
   }
 
   viewer_certificate {
-    acm_certificate_arn = "${aws_acm_certificate.diyasylum_cert.arn}"
+    acm_certificate_arn = aws_acm_certificate.diyasylum_cert.arn
     ssl_support_method = "sni-only"
   }
+
+  depends_on = [aws_acm_certificate_validation.diyasylum_cert_validation]
 }
 
 resource "aws_route53_record" "public_fe_dev_dns" {
-  zone_id = "${data.aws_route53_zone.diyasylum_public.zone_id}"
+  zone_id = data.aws_route53_zone.diyasylum_public.zone_id
   name    = "test.diyasylum.com"
   type    = "CNAME"
   ttl = "5"
-  records = ["${aws_cloudfront_distribution.fe_cloudfront.domain_name}"]
+  records = [aws_cloudfront_distribution.fe_cloudfront.domain_name]
 }
